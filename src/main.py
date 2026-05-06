ebb1dd0414d2b40be9f54f3fc17878b0da994cfd
User <your@email.com>
2026-05-07 00:24:57 +0300

    clean ci cd setup

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/user/1")
-def get_user():
-    return {"id": 1, "name": "Alex"}
-
-@app.get("/user/9999")
-def not_found():
-    return {"detail": "Not found"}
+from fastapi import FastAPI 
+app = FastAPI() 
+ 
+@app.get("/user/1") 
+def get_user(): 
+    return {"id": 1, "name": "Alex"} 
+ 
+@app.get("/user/9999") 
+def not_found(): 
+    return {"detail": "Not found"} 
